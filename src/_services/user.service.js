39b07ccb43f9de0f96baef1c8ea6695a35d3c2a4
Narender1903 <narender1903@gmail.com>
2022-09-17 13:30:45 +0530

    d

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -21,8 +21,8 @@ function login(username, password) {
 
     return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
         .then(handleResponse)
-        .then(user => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
+        .then(user => { 
+            // store user   details and jwt token in local storage to keep user logged in between page refreshes
             localStorage.setItem('user', JSON.stringify(user));
 
             return user;
